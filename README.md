f56f3d84b7a389f579c6ecb102c80ea33779e63e
Kiselevich Andrey <ai_kiselevich@protek.ru>
2020-06-08 20:03:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# django-signup
+Create Advanced User Sign Up View in Django
+
+## Getting Started
+
+Clone the project by selecting right branch and run following commands:
+
+```
+python manage.py makemigrations accounts
+python manage.py migrate
+python manage.py runserver
+
+follow this link: 
+http://127.0.0.1:8000/signup/
+```
---
+++ accounts/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(max_length=100, help_text='User Name')
+    last_name = forms.CharField(max_length=100, help_text='Last Name')
+    email = forms.EmailField(max_length=150, help_text='Email')
+
+    class Meta:
+        model = User
+        fields = ('username', 'last_name', 'email', 'password1', 'password2',)
